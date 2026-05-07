ed4f5b6734e360837cb774b2cde3aba799b630c0
supercoelhao <fernandoazevedogeek@gmail.com>
2026-05-06 21:11:41 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,26 +1,4 @@
 USE dados_clinica;
 
--- Atualiza a foto do Dr. Chopper
-UPDATE medicos 
-SET foto_url = 'imagens/chopper.jpg' 
-WHERE nome LIKE '%Chopper%';
-
--- Atualiza a foto do Dr. Law
-UPDATE medicos 
-SET foto_url = 'imagens/law.jpg' 
-WHERE nome LIKE '%Law%';
-
--- Atualiza a foto da Dra. Tsunade
-UPDATE medicos 
-SET foto_url = 'imagens/tsunade.jpg' 
-WHERE nome LIKE '%Tsunade%';
-
--- Atualiza a foto da Dra. Sakura
-UPDATE medicos 
-SET foto_url = 'imagens/sakura.jpg' 
-WHERE nome LIKE '%Sakura%';
-
--- Atualiza a foto do Dr. Leorio
-UPDATE medicos 
-SET foto_url = 'imagens/leorio.jpg' 
-WHERE nome LIKE '%Leorio%';
+-- Adiciona a coluna medico_id na tabela de agendamentos
+ALTER TABLE agendamentos ADD medico_id INT;
